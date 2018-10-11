00877c8a533f1e50ce715bbd8466874140a28dee
赵卫国 <programfan.info@gmail.com>
2018-10-11 13:11:19 +0800

    upate

---
+++ apps/demo_erlang/src/optimize/demo_optimize_sync.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author zhaoweiguo
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. 十月 2018 上午10:41
+%%%-------------------------------------------------------------------
+-module(demo_optimize_sync).
+-author("zhaoweiguo").
+
+%% API
+-export([index/2]).
+
+-define(SERVER, demo_optimize_server).
+
+
+-spec(index(Flag:: sync | async, N::integer()) -> ok).
+index(Flag, N) ->
+  gen_server:start_link({local, ?SERVER}, ?SERVER, [], []),
+  spawn(doit(Flag, N)),
+  ok.
+
+
+doit(sync, N) ->
+  calls(N).
+
+calls(N) ->
+  call(),
+  calls(N-1).
+
+call() ->
+  gen_server:call(?SERVER, doit).
+
+
+
+
+
